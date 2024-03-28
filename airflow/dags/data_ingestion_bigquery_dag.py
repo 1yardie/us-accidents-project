86430608034d37de847747ed5ad342853d5a56b4
“Hakeem <hakeemjgreen@gmail.com>
2024-03-28 15:46:09 +0000

    initialize new

---
+++ airflow/dags/data_ingestion_bigquery_dag.py
@@ -0,0 +1,16 @@
+import datetime
+
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
+
+
+with DAG(
+    dag_id="data_ingestion_bigquery_dag",
+    start_date=datetime.datetime(2021, 1, 1),
+    schedule="@daily",
+):
+    first_task = EmptyOperator(task_id="first_task")
+    second_task = EmptyOperator(task_id="second_task")
+
+    first_task >> second_task
