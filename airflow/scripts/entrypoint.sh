a5ef8b80922699879eefb40cbf8988f2eb6d8e37
“Hakeem <hakeemjgreen@gmail.com>
2024-04-01 21:36:10 +0000

    Final push

--- airflow/scripts/entrypoint.sh
+++ airflow/scripts/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 export GOOGLE_APPLICATION_CREDENTIALS=${GOOGLE_APPLICATION_CREDENTIALS}
 export AIRFLOW_CONN_GOOGLE_CLOUD_DEFAULT=${AIRFLOW_CONN_GOOGLE_CLOUD_DEFAULT}
-os.environ["KAGGLE_USERNAME"] = "hakeemjgreen"
-os.environ["KAGGLE_KEY"] = "491723fdcc4fa5ff1ce5d8d73d25ccc7" 
+os.environ["KAGGLE_USERNAME"] = ""
+os.environ["KAGGLE_KEY"] = "" 
 airflow db upgrade
 
 airflow users create -r Admin -u admin -p admin -e admin@example.com -f admin -l airflow
